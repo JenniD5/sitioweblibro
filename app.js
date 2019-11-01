fdeab2d54020eb9c44a7197d82614b783a690189
JenniD5 <elizabethduran0105@gmail.com>
2019-10-31 21:10:43 -0700

    responsive

--- app.js
+++ app.js
@@ -7,16 +7,12 @@ const cambio=document.getElementById("caja");
 const libroURL='https://localhost:5001/sagas/libros';
 
 
-let getSimpsonData3= async()=>{
-    const response=await fetch (`${libroURL}/0`);
+let getSimpsonData3= async ()=>{
+    const response=await fetch (libroURL);
     const libros=await response.json();
     console.log(libros);
     const{titulo}=libros;
-    nombre3.innerText=titulo;
-    app.update()
-         
-   
-     
+    nombre3.innerText=titulo;    
 };
 
 getSimpsonData3();
